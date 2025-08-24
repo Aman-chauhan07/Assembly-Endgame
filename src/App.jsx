03875fc2aac523253df151d483e53d537f5c6700
Aman-chauhan07 <amanx1class@gmail.com>
2025-08-24 15:12:57 +0530

    #commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
-import Header from './Header'
+import { languages } from './Language'
 
-const App = () => {
+function AssemblyEndgame() {
+
+  const languageElements = languages.map(lang => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color
+    }
+    return (
+      <span className="chip"
+        style={styles}
+        key={lang.name}
+        >
+        {lang.name}
+      </span>
+    )
+  })
   return (
-    <div>
-      <Header/>
-    </div>
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word within 8 attempts to keep the
+          programming world safe from Assembly!</p>
+      </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className='language-chips'>
+        {languageElements}
+      </section>
+
+    </main>
   )
 }
 
-export default App
+export default AssemblyEndgame

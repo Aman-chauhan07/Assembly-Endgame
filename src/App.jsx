fbc27c5e46ad694b81d0948dd55149566afa2166
Aman-chauhan07 <amanx1class@gmail.com>
2025-08-26 16:30:42 +0530

    #Keyboard functionality change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { languages } from './Language'
+import clsx from "clsx"
+
 
 function AssemblyEndgame() {
-  const [currentWord, setCurrentWord] = useState("react")
+    const [currentWord, setCurrentWord] = useState("react")
     const [guessedLetters, setGuessedLetters] = useState([])
-    console.log(guessedLetters)
 
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
@@ -16,6 +17,7 @@ function AssemblyEndgame() {
         })
     }
 
+    // its render the languages button / languages...
     const languageElements = languages.map(lang => {
         const styles = {
             backgroundColor: lang.backgroundColor,
@@ -32,41 +34,56 @@ function AssemblyEndgame() {
         )
     })
 
+    // jo bhi user button click krega vo button show karge screen par...
     const letterElements = currentWord.split("").map((letter, index) => (
         <span key={index}>{letter.toUpperCase()}</span>
     ))
 
-    const keyboardElements = alphabet.split("").map(letter => (
-        <button
-            key={letter}
-            onClick={() => addGuessedLetter(letter)}
-        >
-            {letter.toUpperCase()}
-        </button>
-    ))
+    // It render the keyboard in screen...
+    const keyboardElements = alphabet.split("").map(letter => {
+        const isInclude = guessedLetters.includes(letter)
+        const iscorrect = isInclude && currentWord.includes(letter)
+        const iswrong = isInclude && !currentWord.includes(letter)
+        const className = clsx({
+            correct :iscorrect,
+            wrong : iswrong
+        })
 
-    return (
-        <main>
-            <header>
-                <h1>Assembly: Endgame</h1>
-                <p>Guess the word within 8 attempts to keep the
-                programming world safe from Assembly!</p>
-            </header>
-            <section className="game-status">
-                <h2>You win!</h2>
-                <p>Well done! 🎉</p>
-            </section>
-            <section className="language-chips">
-                {languageElements}
-            </section>
-            <section className="word">
-                {letterElements}
-            </section>
-            <section className="keyboard">
-                {keyboardElements}
-            </section>
-            <button className="new-game">New Game</button>
-        </main>
-    )
+
+        return(
+            <button
+                className = { className } 
+                key = { letter }
+                onClick = {() => addGuessedLetter(letter)
 }
- export default AssemblyEndgame
+            >
+    { letter.toUpperCase() }
+            </button >
+        )
+    })
+
+return (
+    <main>
+        <header>
+            <h1>Assembly: Endgame</h1>
+            <p>Guess the word within 8 attempts to keep the
+                programming world safe from Assembly!</p>
+        </header>
+        <section className="game-status">
+            <h2>You win!</h2>
+            <p>Well done! 🎉</p>
+        </section>
+        <section className="language-chips">
+            {languageElements}
+        </section>
+        <section className="word">
+            {letterElements}
+        </section>
+        <section className="keyboard">
+            {keyboardElements}
+        </section>
+        <button className="new-game">New Game</button>
+    </main>
+)
+}
+export default AssemblyEndgame
